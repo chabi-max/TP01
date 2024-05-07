c17db47fff44c2e577efc866544a83ee05a2df4d
Jean Bonilla <e2171156@cmaisonneuve.qc.ca>
2024-05-07 11:19:08 -0400

    ajout en-tete

--- TP02.py
+++ TP02.py
@@ -1,3 +1,10 @@
+'''
+Travail Pratique 2
+Jean-Hernan Bonilla et Maksen Chabi
+e2171156 et e2362443
+**-05-2024
+'''
+
 with open('Donnees.csv','r',encoding='utf-8') as f:
     print(f.read())
     
