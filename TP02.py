b86e65e7a271e4187a8724c38c1c012c49b83aab
maksen chabi <e2362443@cmaisonneuve.qc.ca>
2024-05-20 17:01:00 -0400

    finalisation de code pour les classes

--- TP02.py
+++ TP02.py
@@ -2,7 +2,7 @@
 Travail Pratique 2
 Jean-Hernan Bonilla et Maksen Chabi
 e2171156 et e2362443
-**-05-2024
+20-05-2024
 '''
 import csv
 import json
@@ -74,41 +74,4 @@ def trouverDistanceMin(nomFichier):
         writer.writerow([f'{ville1.ville}, {ville1.pays}', f'{ville2.ville}, {ville2.pays}', min_distance])
 
     print(
-        f"Distance minimale en kilomètres entre 2 villes : Ville 1 : {ville1.ville} {ville1.pays} {ville1.latitude} {ville1.longitude} et Ville 2 : {ville2.ville} {ville2.pays} {ville2.latitude} {ville2.longitude} Distance en kilomètres : {min_distance}")
-
-
-def afficher_menu():
-    print("1- Lire les données du fichier csv, créer les objets et afficher les données.")
-    print("2- Sauvegarder les données dans un fichier .json.")
-    print(
-        "3- Lire les données du fichier .json, déterminer et afficher les données associées à la distance minimale entre deux villes et sauvegarder les calculs dans distances.csv.")
-    print("Entrez un numéro pour choisir une option ou appuyez sur 'q' pour quitter :")
-
-
-def main():
-    listeObjDonneesGeo = []
-    while True:
-        afficher_menu()
-        choix = input()
-        if choix == '1':
-            listeObjDonneesGeo = lireDonneesCsv('Donnees.csv')
-            for obj in listeObjDonneesGeo:
-                print(obj)
-            input("Appuyez sur une touche pour continuer...")
-        elif choix == '2':
-            if listeObjDonneesGeo:
-                ecrireDonneesJson('Donnees.json', listeObjDonneesGeo)
-            else:
-                print("Veuillez d'abord lire les données du fichier CSV (Option 1).")
-        elif choix == '3':
-            if listeObjDonneesGeo:
-                trouverDistanceMin('Donnees.json')
-            else:
-                print("Veuillez d'abord sauvegarder les données dans un fichier JSON (Option 2).")
-        elif choix == 'q':
-            break
-        else:
-            print("Choix non valide. Veuillez réessayer.")
-
-if __name__ == "__main__":
-    main()
+        f"Distance minimale en kilomètres entre 2 villes : Ville 1 : {ville1.ville} {ville1.pays} {ville1.latitude} {ville1.longitude} et Ville 2 : {ville2.ville} {ville2.pays} {ville2.latitude} {ville2.longitude} Distance en kilomètres : {min_distanc
